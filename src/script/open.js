0347cf9be0d1f626de70449d75cb165a0ed9f35f
Eksens <nazararius@gmail.com>
2025-02-11 23:53:55 +0100

    open js test

---
+++ src/script/open.js
@@ -0,0 +1,6 @@
+function openPage(pageNumber) {
+  window.location.href = `content.html?page=${pageNumber}`;
+}
+window.openPage = function (pageNumber) {
+  window.location.href = `content.html?page=${pageNumber}`;
+};
