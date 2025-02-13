14d2f3fac26092a31e796db239865d4979d47bed
Eksens <45803167+Eksens@users.noreply.github.com>
2025-02-13 19:56:04 +0000

    Deploying to gh-pages from @ makuusik/panmodull@fd40f2f2bf12d3343e0c0fc9a1759ce6a722a9fa 🚀

--- gallery.js
+++ gallery.js
@@ -1,2 +1,2 @@
-import"./assets/header_script-DAFMYebC.js";document.addEventListener("DOMContentLoaded",()=>{const o=document.querySelectorAll("video"),r=new IntersectionObserver(e=>{e.forEach(t=>{t.isIntersecting?t.target.play():t.target.pause()})},{threshold:.5});o.forEach(e=>r.observe(e))});
+import"./assets/header_script-CsLR9MqL.js";document.addEventListener("DOMContentLoaded",()=>{const o=document.querySelectorAll("video"),r=new IntersectionObserver(e=>{e.forEach(t=>{t.isIntersecting?t.target.play():t.target.pause()})},{threshold:.5});o.forEach(e=>r.observe(e))});
 //# sourceMappingURL=gallery.js.map
